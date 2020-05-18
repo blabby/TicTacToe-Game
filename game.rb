98c39ecedc6ec877d09a017ed6d5a76aa45f0975
James Grieve <pocojames@hotmail.com>
2020-05-18 13:47:17 -0700

    All win conditions made, V1 complete, single player tictactoe

--- game.rb
+++ game.rb
@@ -1,106 +1,96 @@
-#You can enter the size of the board (EX: 3x3 or 10x10 grid)
-#You can have 2 or more players (human controlled or computer controlled)
 class Board
-    attr_reader :size
-
-    def initalize
+    attr_reader :size, :board, :sym
+    def initialize
         puts "Enter game size:"
         @size = gets.chomp.to_i
-        #Enter human player names EX: James Chris Steven --> 3
-        #Enter computer player names EX: Matt Jessica Athur --> 3
-        #...each do |el| Player.new(el)   shove that into players array
-        puts "Enter amount of human players followed by computer players Ex: 1 1 (one human one computer)"
-        @amt = gets.chomp.split(" ").map(&:to_i)
-        @human_players = @amt.first
-        @computer_players = @amt.last
-        @players = []
-        @board = Array.new(size) {Array.new(size, "_")}
-    end
-
-    def initialize_players
-
+        @board = Array.new(@size) {Array.new(@size, "_")}
+        puts "Enter your symbol"
+        @sym = gets.chomp.to_sym
     end
 
     def run
-        puts "Welcome to TicTacToe"
-        until win?(sym) || lose?
+        puts "Welcome to TicTacToe!"
+        until win?(@sym) || lose?
             self.display
+            puts "Enter position. (EX: 0 0 for position 1 on board)"
+            pos = gets.chomp.split(" ").map(&:to_i)
+            while !legal_position?(pos)
+                puts "Invalid position, please try again. (EX: 0 0 for position 1 on board)"
+                pos = gets.chomp.split(" ").map(&:to_i)
+            end
+            self.placement(pos)
         end
+        self.display
         puts "GAME OVER!"
+    end
+
+    def placement(pos)
+        @board[pos[0]][pos[1]] = @sym
     end
 
     def display
         @board.each do |row|
-            puts row
-        end
-    end
-
-    def switch_players
-
+            p row
+        end     
     end
 
     def win?(sym)
-        return true if diagonal_win?(sym) || vertical_win?(sym) || horizontal_win?(sym)
+         if diagonal_win?(sym) || vertical_win?(sym) || horizontal_win?(sym)
+            puts "YOU WIN!!!!!"
+        return true
+         end
         false
     end
 
     def lose?
         @board.each do |array|
-            return true if array.none? {|el| el == "_"}
+             if array.none? {|el| el == "_"}
+            puts "YOU LOSE!!!!!!!"
+            return true
+        end
         end
         false
     end
 
     def diagonal_win?(sym)
         return true if left_to_right(sym) || right_to_left(sym)
+        false
     end
 
-    def left_to_right(sym, reversed_board = nil)
-        array = []
-
-        if reversed_board
-            reversed_board.each_with_index do |row, i1|
-                @row.each_with_index do |col, i2|
-                    if i1 == i2 && reversed_board[i1][i2] == sym
-                        array << sym
-                    end
-                end
-            end
-                if array.length == @board.length
-                    return true
-                end
-                false
-        else
+    def left_to_right(sym)
+        count = 0
         @board.each_with_index do |row, i1|
-            @row.each_with_index do |col, i2|
+            row.each_with_index do |col, i2|
                 if i1 == i2 && @board[i1][i2] == sym
-                    array << sym
+                    count += 1
                 end
             end
         end
-            if array.length == @board.length
-                return true
-            end
-            false
-        end
+        return true if count == @board.length
+        false
     end
-
 
     def right_to_left(sym)
-        reversed_board = []
-        
-        @board.each do |row|
-            reversed_board << row.reverse
+        flipped_array = []
+        @board.each {|el| flipped_array << el.reverse}
+        count = 0
+        flipped_array.each_with_index do |row, i1|
+            row.each_with_index do |col, i2|
+                if i1 == i2 && flipped_array[i1][i2] == sym
+                    count += 1
+                end
+            end
         end
-
-        left_to_right(sym, reversed_board)
+        return true if count == @board.length
+        false
     end
 
     def vertical_win?(sym)
         transposed_array = @board.transpose
-        @transposed_array.each do |array|
+        transposed_array.each do |array|
             return true if array.all? {|el| el == sym}
         end
+        false
     end
 
     def horizontal_win?(sym)
@@ -111,10 +101,11 @@ class Board
     end
 
     def legal_position?(pos)
-        @pos.each do |row, col|
-            return false if @board[row][col] != "_" || !@board[row][col]
+        if @board[pos[0]][pos[1]] != "_" || @board[pos[0]][pos[1]] == nil
+            return false
         end
-        false
+        true
     end
-    #keep giving opponent guess (while loop in run method) while legal position returns false
 end
+
+Board.new.run
