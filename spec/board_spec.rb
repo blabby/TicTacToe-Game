f82055c53913bc2bd14231863987a020d8af24f2
James Grieve <pocojames@hotmail.com>
2020-08-08 12:39:37 -0700

    add more tests to board, win? lose? and initialize methods

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,24 +3,59 @@ require "./lib/board.rb"
 describe Board do
     describe "#initialize" do
     it "it should have an instance variable for board size" do
-        end
-
-    it "it should have an instance variable for human players" do
-        end
-
-    it "it should have an instance variable for computer players" do
+        brd = Board.new
+        expect(brd.size).to eql(3) #enter 3 for input
         end
 
     it "it should have an instance variable for the board depending on size" do
+        brd = Board.new
+        expect(brd.board.length).to eql(9) #equal to brd.size * brd.size
+    end
+
+    it "it should have an instance variable for the number of computer players" do
+        brd = Board.new
+        expect(brd.computer).to eql(1) #enter 1 for input
+    end
+
+    it "it should have an instance variable for the number of human players" do
+        brd = Board.new
+        expect(brd.human).to eql(1) #enter 1 for input
+    end
+
+    it "it should have an instance array for human players" do
+        brd = Board.new
+        expect(brd.players.length).to eql(1) #enter 1 for input
+        end
+
+    it "it should have an instance variable for computer players" do
+        brd = Board.new
+        expect(brd.computer).to eql(1) #enter 1 for input
         end
     end
 
-    describe "#win?" do
+    describe "#win?" do #to test these conditions place tiles to achieve these conditions
+        it "it should return false if win conditions aren't met" do
+            brd = Board.new
+            expect(brd.win?).to eql(true) 
+        end
 
+        it "it should return true if there is a horizontal win" do
+            brd = Board.new
+            expect(brd.win?).to eql(true) 
+        end
+
+        it "it should return true if there is a vertical win" do
+            brd = Board.new
+            expect(brd.win?).to eql(true) 
+        end
+
+        it "it should return true if there is a diagonal win" do
+            brd = Board.new
+            expect(brd.win?).to eql(true) 
+        end 
     end
 
     describe "#lose?" do
-
     end 
 
 
