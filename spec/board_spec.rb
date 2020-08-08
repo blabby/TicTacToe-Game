bbfb38bae940fcecbcd89db55e2e706530f87f22
James Grieve <pocojames@hotmail.com>
2020-08-07 23:25:21 -0700

    added some board specs

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,11 +2,31 @@ require "./lib/board.rb"
 
 describe Board do
     describe "#initialize" do
-    it "" do
-
+    it "it should have an instance variable for board size" do
         end
 
-    it "" do
+    it "it should have an instance variable for human players" do
+        end
+
+    it "it should have an instance variable for computer players" do
+        end
+
+    it "it should have an instance variable for the board depending on size" do
+        end
+    end
+
+    describe "#win?" do
+
+    end
+
+    describe "#lose?" do
+
+    end 
+
+
+
+    describe "#run" do
+    it "should continue until win? or lose? conditions met" do
         end
     end
 end
